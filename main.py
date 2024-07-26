deb9f1ccd6559fab0baa3afbdaefb54ba7805aa0
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-07-26 16:06:12 +0000

    calculo de media

---
+++ main.py
@@ -0,0 +1,16 @@
+# n1 = "Sabina\n"
+# n2 = "Oliveira"
+
+# print(n1+n2)
+
+# calculo de média
+# usei o int para transformar a string do input em inteiro
+# n1 = int(input("Digite o primeiro número: "))
+# n2 = int(input("Digite o segundo número: "))
+# soma = n1 + n2
+# media = (n1 + n2)/2
+# print("A soma dos valores é: ",soma," e a média é: ",media)
+
+# def greeting(name):
+#     print("Boas vindas, " + name + "!!!")
+# greeting("sabina")
