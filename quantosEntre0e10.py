83f5fcf9e227d3fc3d624e9adb542591e80dd8ae
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-08-03 01:01:47 +0000

    atualização

--- quantosEntre0e10.py
+++ quantosEntre0e10.py
@@ -4,7 +4,8 @@ for contador in range(6):
     valor = int(input("Digite um valor: "))
     if valor >= 0 and valor <= 10:
         total = total + 1
-        if valor%2 = 1:
-            somaImp = SomaImp + valor
+        if valor %2 == 1:
+            somaImp = somaImp + valor
 print("Ao todo foram",total,"valores entre 0 e 10")
+print("Nesse intervalo, a soma de ímpares foi: ",somaImp)
 
