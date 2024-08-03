83dc3ceae6397869d033509df65fd21b3c72f702
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-08-03 04:21:48 +0000

    atualização

---
+++ combinacoes.py
@@ -0,0 +1,3 @@
+for contador1 in range(1,3 + 1):
+    for contador2 in range(1,3 + 1):
+        print(contador1,contador2)
---
+++ fazSomaProcedimento.py
@@ -0,0 +1,5 @@
+def soma(num1,num2):
+    return num1 + num2
+numero1 = int(input("Qual o valor? "))
+numero2 = int(input("Qual o segundo valor? "))
+print("A soma é:",soma(numero1,numero2))
