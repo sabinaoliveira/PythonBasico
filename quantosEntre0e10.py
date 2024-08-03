fef38ceb420b077e16085064632bee9a38eb57af
Sabina <104401758+sabinaoliveira@users.noreply.github.com>
2024-08-03 00:51:36 +0000

    atualização

---
+++ quantosEntre0e10.py
@@ -0,0 +1,10 @@
+total = 0
+somaImp = 0
+for contador in range(6):
+    valor = int(input("Digite um valor: "))
+    if valor >= 0 and valor <= 10:
+        total = total + 1
+        if valor%2 = 1:
+            somaImp = SomaImp + valor
+print("Ao todo foram",total,"valores entre 0 e 10")
+
---
+++ valoresPares.py
@@ -0,0 +1,5 @@
+valor = int(input("Digite um valor: "))
+for contador in range(0,valor,2):
+    print(contador)
+
+
